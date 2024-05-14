3e96633fe985a50c0f738bf094a9d5c502e62443
kettle12110 <kettle12110@gmail.com>
2024-05-14 21:51:30 +0900

    途中

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const btn =document.querySelector('#btn');
 btn.addEventListener('click',()=>{
-    document.body.classList.toggle('light-theme');
     if(btn.textContent ==='ライトモードにする'){
         btn.textContent ='ダークモードにする'
+        document.body.classList.replace('main-color-dark','main-color-light');
+        document.body.classList.replace('sub-color-dark','sub-color-light');
     }else{
         btn.textContent='ライトモードにする';
+        document.body.classList.replace('main-color-light','main-color-dark');
+        document.body.classList.replace('sub-color-light','sub-color-dark');
         }
 });
