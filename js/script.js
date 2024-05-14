445ee6a66113b208aa18b2964e0d63315aa63971
kettle12110 <kettle12110@gmail.com>
2024-05-15 00:44:50 +0900

    update：body以外のクラス変える方法を考え中

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-const btn =document.querySelector('#btn');
-btn.addEventListener('click',()=>{
-    if(btn.textContent ==='ライトモードにする'){
-        btn.textContent ='ダークモードにする'
-        document.body.classList.replace('main-color-dark','main-color-light');
-        document.body.classList.replace('sub-color-dark','sub-color-light');
-    }else{
-        btn.textContent='ライトモードにする';
-        document.body.classList.replace('main-color-light','main-color-dark');
-        document.body.classList.replace('sub-color-light','sub-color-dark');
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    if (btn.textContent === 'ライトモードにする') {
+        btn.textContent = 'ダークモードにする'
+        document.body.classList.replace('main-color-dark', 'main-color-light');
+
+        let classes = document.getElementsByClassName("sub-color-dark");
+        for (let i = 0; i < classes.length; i++) {
+            classes[i].classList.add("sub-color-light");
         }
+        for (let i = 0; i < classes.length; i++) {
+            classes[i].classList.remove("sub-color-dark");
+        }
+    } else {
+        btn.textContent = 'ライトモードにする';
+        document.body.classList.replace('main-color-light', 'main-color-dark');
+
+        let classes = document.getElementsByClassName("sub-color-light");
+        for (let i = 0; i < classes.length; i++) {
+            classes[i].classList.add("sub-color-dark");
+        }
+        for (let i = 0; i < classes.length; i++) {
+            classes[i].classList.remove("sub-color-light");
+        }
+    }
 });
