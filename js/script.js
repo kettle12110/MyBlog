419d2b0e788c40bcf43763a9edf9f94026b8156a
kettle12110 <kettle12110@gmail.com>
2024-05-15 21:16:27 +0900

    フレンズが教えてくれたやつ

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
+//フレンズが教えてくれたやつ
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
     if (btn.textContent === 'ライトモードにする') {
-        btn.textContent = 'ダークモードにする'
+        btn.textContent = 'ダークモードにする';
         document.body.classList.replace('main-color-dark', 'main-color-light');
+        document.body.classList.replace('sub-color-dark', 'sub-color-light');
 
-        let classes = document.getElementsByClassName("sub-color-dark");
-        for (let i = 0; i < classes.length; i++) {
-            classes[i].classList.add("sub-color-light");
-        }
-        for (let i = 0; i < classes.length; i++) {
-            classes[i].classList.remove("sub-color-dark");
-        }
+        // 他の要素のクラスを置き換える
+        document.querySelectorAll('.sub-color-dark').forEach(element => {
+            element.classList.replace('sub-color-dark', 'sub-color-light');
+        });
+        document.querySelectorAll('.main-color-dark').forEach(element => {
+            element.classList.replace('main-color-dark', 'main-color-light');
+        });
     } else {
         btn.textContent = 'ライトモードにする';
         document.body.classList.replace('main-color-light', 'main-color-dark');
+        document.body.classList.replace('sub-color-light', 'sub-color-dark');
 
-        let classes = document.getElementsByClassName("sub-color-light");
-        for (let i = 0; i < classes.length; i++) {
-            classes[i].classList.add("sub-color-dark");
-        }
-        for (let i = 0; i < classes.length; i++) {
-            classes[i].classList.remove("sub-color-light");
-        }
+        // 他の要素のクラスを置き換える
+        document.querySelectorAll('.sub-color-light').forEach(element => {
+            element.classList.replace('sub-color-light', 'sub-color-dark');
+        });
+        document.querySelectorAll('.main-color-light').forEach(element => {
+            element.classList.replace('main-color-light', 'main-color-dark');
+        });
     }
 });
