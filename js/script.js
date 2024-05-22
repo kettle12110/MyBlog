7a228314b918d71f9008e2d6c7f3be65378dd599
kettle12110 <kettle12110@gmail.com>
2024-05-22 23:49:51 +0900

    update：関数でリファクタ

--- js/script.js
+++ js/script.js
@@ -1,29 +1,20 @@
-//フレンズが教えてくれたやつ
+function replaceClass(currentColor, ChangedColor){
+document.querySelectorAll('.' + currentColor).forEach(element => {
+    element.classList.replace(currentColor, ChangedColor);
+});
+}
+
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
     if (btn.textContent === 'ライトモードにする') {
         btn.textContent = 'ダークモードにする';
-        document.body.classList.replace('main-color-dark', 'main-color-light');
-        document.body.classList.replace('sub-color-dark', 'sub-color-light');
-
         // 他の要素のクラスを置き換える
-        document.querySelectorAll('.sub-color-dark').forEach(element => {
-            element.classList.replace('sub-color-dark', 'sub-color-light');
-        });
-        document.querySelectorAll('.main-color-dark').forEach(element => {
-            element.classList.replace('main-color-dark', 'main-color-light');
-        });
+        replaceClass('main-color-dark', 'main-color-light')
+        replaceClass('sub-color-dark', 'sub-color-light')
     } else {
         btn.textContent = 'ライトモードにする';
-        document.body.classList.replace('main-color-light', 'main-color-dark');
-        document.body.classList.replace('sub-color-light', 'sub-color-dark');
-
-        // 他の要素のクラスを置き換える
-        document.querySelectorAll('.sub-color-light').forEach(element => {
-            element.classList.replace('sub-color-light', 'sub-color-dark');
-        });
-        document.querySelectorAll('.main-color-light').forEach(element => {
-            element.classList.replace('main-color-light', 'main-color-dark');
-        });
+        replaceClass('main-color-light', 'main-color-dark')
+        replaceClass('sub-color-light', 'sub-color-dark')
+        };
     }
-});
+);
