a5adf0d7057b788a08fbc1cab23c81c332016f0d
kettle12110 <kettle12110@gmail.com>
2024-06-01 19:13:30 +0900

    プログレスバー、レイアウト修正

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const getScrollPercent =()=>{
+    const scrolled =window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight;
+    const viewHeight = document.documentElement.clientHeight;
+    const percentage = scrolled /(pageHeight - viewHeight)*100;
+    document.querySelector('#bar').style.width=`${percentage}%`;
+}
+window.addEventListener('scroll',getScrollPercent);
+
 function replaceClass(currentColor, ChangedColor){
 document.querySelectorAll('.' + currentColor).forEach(element => {
     element.classList.replace(currentColor, ChangedColor);
